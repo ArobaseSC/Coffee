e48e4a14b082da7c51e0eb400ff8bd4f318fb6ef
Alexis C. Gridel <contact@agdl.dev>
2022-09-22 13:31:42 +0200

    feat(root): introduce recursive decompression

---
+++ coffee/src/main/java/org/arobase/service/RecurseDecompressor.java
@@ -0,0 +1,33 @@
+package org.arobase.service;
+
+/****************************************************
+ *    Copyright (c) 2022 — Arobase Team
+ *
+ *    This project may be licensed somewhere,
+ *    you may want to check the license file in the GitHub root repository.
+ *
+ *    All rights reserved to the Arobase team members.
+ ****************************************************/
+
+import org.arobase.service.factory.DecompressorServiceFactory;
+
+/**
+ * Represents the main recurse abstraction service to decompress something recursively.
+ * <p>
+ * It should not be use directly but through the {@link DecompressorServiceFactory}.
+ * The factory will return the right implementation of this service based on your needs.
+ * For example, if you want to decompress a folder of zip files, you should use the {@link RecurseZipDecompressorService}.
+ * </p>
+ *
+ * @author Alexis Chân Gridel
+ */
+public interface RecurseDecompressor extends DecompressorService {
+
+	/**
+	 * Decompress all files inside the given folder to the given destination.
+	 *
+	 * @param source      the folder where to find files to decompress
+	 * @param destination the destination where to decompress the files
+	 */
+	void decompress(final String source, final String destination);
+}
---
+++ coffee/src/main/java/org/arobase/service/RecurseZipDecompressorService.java
@@ -0,0 +1,14 @@
+package org.arobase.service;
+
+/****************************************************
+ *    Copyright (c) 2022 — Arobase Team
+ *
+ *    This project may be licensed somewhere,
+ *    you may want to check the license file in the GitHub root repository.
+ *
+ *    All rights reserved to the Arobase team members.
+ ****************************************************/
+
+public class RecurseZipDecompressorService {
+
+}
