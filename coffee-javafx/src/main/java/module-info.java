234b7deef3d4d950d54111cdbe8cee7a61f74c5e
Alexis C. Gridel <contact@agdl.dev>
2022-09-22 13:35:10 +0200

    feat(jfx): replace to module instead of project

--- coffee-javafx/src/main/java/module-info.java
+++ coffee-javafx/src/main/java/module-info.java
@@ -1,11 +1,15 @@
 module org.arobase.coffeejavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
+	requires org.controlsfx.controls;
+	requires com.dlsc.formsfx;
+	requires validatorfx;
+	requires org.kordamp.ikonli.javafx;
+	requires org.kordamp.bootstrapfx.core;
+	requires eu.hansolo.tilesfx;
 
-    opens org.arobase.coffeejavafx to javafx.fxml;
-    exports org.arobase.coffeejavafx;
+	opens org.arobase.coffeejavafx to javafx.fxml;
+	exports org.arobase.coffeejavafx;
 }
