b61a541106cc888cd2be078d3b11eaea4918e6bd
Alexis C. Gridel <contact@agdl.dev>
2022-09-23 16:34:31 +0200

    fix(root): fix indentation spaces instead of tab

--- coffee/src/test/java/org/arobase/ResultTest.java
+++ coffee/src/test/java/org/arobase/ResultTest.java
@@ -19,37 +19,37 @@ import org.junit.jupiter.api.Test;
 
 class ResultTest {
 
-	private Integer nullIfIEqualsFive(final int i) {
-		if (i == 5) {
-			return null;
-		}
+    private Integer nullIfIEqualsFive(final int i) {
+        if (i == 5) {
+            return null;
+        }
 
-		return i;
-	}
+        return i;
+    }
 
-	@Test
-	@DisplayName("Null object should be a failure result")
-	void returnResultObjectOfNullShouldBeFailure() {
-		// Given
-		final var i = 5;
+    @Test
+    @DisplayName("Null object should be a failure result")
+    void returnResultObjectOfNullShouldBeFailure() {
+        // Given
+        final var i = 5;
 
-		// When
-		final var result = Result.from(nullIfIEqualsFive(i));
+        // When
+        final var result = Result.from(nullIfIEqualsFive(i));
 
-		// Then
-		assertThat(result).isInstanceOf(FailureResult.class);
-	}
+        // Then
+        assertThat(result).isInstanceOf(FailureResult.class);
+    }
 
-	@Test
-	@DisplayName("Non Null object should be a success result")
-	void returnResultObjectOfNonNullShouldBeSuccess() {
-		// Given
-		final var i = 2;
+    @Test
+    @DisplayName("Non Null object should be a success result")
+    void returnResultObjectOfNonNullShouldBeSuccess() {
+        // Given
+        final var i = 2;
 
-		// When
-		final var result = Result.from(nullIfIEqualsFive(i));
+        // When
+        final var result = Result.from(nullIfIEqualsFive(i));
 
-		// Then
-		assertThat(result).isInstanceOf(SuccessResult.class);
-	}
+        // Then
+        assertThat(result).isInstanceOf(SuccessResult.class);
+    }
 }
