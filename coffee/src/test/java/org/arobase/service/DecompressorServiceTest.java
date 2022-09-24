b3e864fc337732774b3099f2089181c4b805b684
Alexis C. Gridel <contact@agdl.dev>
2022-09-24 06:49:49 +0200

    feat(root): test with file content and test ensure

--- coffee/src/test/java/org/arobase/service/DecompressorServiceTest.java
+++ coffee/src/test/java/org/arobase/service/DecompressorServiceTest.java
@@ -8,8 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 /****************************************************
  *    Copyright (c) 2022 — Arobase Team
@@ -29,7 +33,16 @@ class DecompressorServiceTest {
 
     @BeforeEach
     void setUp() throws IOException {
-        temporaryGeneratedZipFile = File.createTempFile("test", ".zip", temporaryFolder);
+        temporaryGeneratedZipFile = Files.createTempFile(temporaryFolder.toPath(), "test", ".zip")
+            .toFile();
+
+        try (final var zipOutputStream = new ZipOutputStream(
+            new FileOutputStream(temporaryGeneratedZipFile))) {
+            final var zipEntry = new ZipEntry("quizio.txt");
+
+            zipOutputStream.putNextEntry(zipEntry);
+        }
+
         temporaryGeneratedOutputDirectory = Files.createDirectory(
             temporaryFolder.toPath().resolve("output")).toFile();
     }
@@ -56,5 +69,15 @@ class DecompressorServiceTest {
     @DisplayName("The temporary output folder should be empty")
     void ensureTemporaryOutputFolderIsEmpty() {
         assertThat(temporaryGeneratedOutputDirectory).isEmptyDirectory();
+    }
+
+    @Test
+    @DisplayName("The zip file should contain a file named quizio.txt")
+    void ensureZipFileContainsAFileNamedQuizioTxt() {
+        try (final var zipFile = new ZipFile(temporaryGeneratedZipFile)) {
+            assertThat(zipFile.getEntry("quizio.txt")).isNotNull();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
 }
