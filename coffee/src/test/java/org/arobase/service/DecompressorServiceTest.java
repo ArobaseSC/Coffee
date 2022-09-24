066041b2d5474c8e01e02055e134b99823ee2ff4
Alexis C. Gridel <contact@agdl.dev>
2022-09-24 06:24:25 +0200

    feat(root): rule for gen temp zip and dir file exist

--- coffee/src/test/java/org/arobase/service/DecompressorServiceTest.java
+++ coffee/src/test/java/org/arobase/service/DecompressorServiceTest.java
@@ -2,10 +2,14 @@ package org.arobase.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /****************************************************
  *    Copyright (c) 2022 — Arobase Team
@@ -18,11 +22,33 @@ import java.io.File;
 
 class DecompressorServiceTest {
 
+    protected File temporaryGeneratedZipFile;
+    protected File temporaryGeneratedOutputDirectory;
     @TempDir
     private File temporaryFolder;
 
+    @BeforeEach
+    void setUp() throws IOException {
+        temporaryGeneratedZipFile = File.createTempFile("test", ".zip", temporaryFolder);
+        temporaryGeneratedOutputDirectory = Files.createDirectory(
+            temporaryFolder.toPath().resolve("output")).toFile();
+    }
+
     @Test
+    @DisplayName("The temporary folder should exists")
     void ensureTemporaryFolderExists() {
         assertThat(temporaryFolder).exists();
+    }
+
+    @Test
+    @DisplayName("The temporary generated zip file should exists")
+    void ensureTemporaryGeneratedZipFileExists() {
+        assertThat(temporaryGeneratedZipFile).exists();
+    }
+
+    @Test
+    @DisplayName("The temporary generated output directory should exists")
+    void ensureTemporaryGeneratedOutputDirectoryExists() {
+        assertThat(temporaryGeneratedOutputDirectory).exists();
     }
 }
