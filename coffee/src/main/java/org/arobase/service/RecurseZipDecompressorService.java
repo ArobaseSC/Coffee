a0b221244b88a9db23f7a970c7c39fa45140ed04
Alexis C. Gridel <contact@agdl.dev>
2022-09-24 14:11:40 +0200

    fix(abstraction): abstract inheritance from concrete impl of recurse version of Decompressor Service

--- coffee/src/main/java/org/arobase/service/RecurseZipDecompressorService.java
+++ coffee/src/main/java/org/arobase/service/RecurseZipDecompressorService.java
@@ -1,5 +1,24 @@
 package org.arobase.service;
 
-public class RecurseZipDecompressorService {
+import org.arobase.abstraction.Result;
 
+import java.io.File;
+
+/**
+ *
+ */
+public class RecurseZipDecompressorService implements RecurseDecompressor {
+
+    /**
+     * Decompress all files inside the given folder to the given destination.
+     *
+     * @param source      the folder where to find files to decompress
+     * @param destination the destination where to decompress the files
+     * @return a {@link Result} that will be a success if the decompression succeed,
+     * otherwise it will be an error
+     */
+    @Override
+    public Result decompress(final File source, final File destination) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
