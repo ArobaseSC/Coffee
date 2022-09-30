d6f39f40112d64664b6dfbb5e2ae04d105e72846
Alexis C. Gridel <contact@agdl.dev>
2022-09-30 13:59:46 +0200

    feat(core-test): decompress recursive not directory should be failure result

--- coffee/src/test/java/org/arobase/service/recursive/RecurseZipDecompressorServiceTest.java
+++ coffee/src/test/java/org/arobase/service/recursive/RecurseZipDecompressorServiceTest.java
@@ -1,5 +1,7 @@
 package org.arobase.service.recursive;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.arobase.abstraction.Result;
 import org.arobase.service.RecurseZipDecompressorService;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,14 +9,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.beans.Transient;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class RecurseZipDecompressorServiceTest {
 
@@ -48,47 +49,52 @@ class RecurseZipDecompressorServiceTest {
 
     @BeforeEach
     void setUp() throws IOException {
-        this.temporaryGeneratedZipFile1 = Files.createTempFile(this.temporaryFolder.toPath(), "test1",
-                ".zip").toFile();
+        this.temporaryGeneratedZipFile1 = Files.createTempFile(this.temporaryFolder.toPath(),
+            "test1",
+            ".zip").toFile();
 
-        this.temporaryGeneratedZipFile2 = Files.createTempFile(this.temporaryFolder.toPath(), "test2",
-                ".zip").toFile();
+        this.temporaryGeneratedZipFile2 = Files.createTempFile(this.temporaryFolder.toPath(),
+            "test2",
+            ".zip").toFile();
 
-        this.temporaryGeneratedZipFile3 = Files.createTempFile(this.temporaryFolder.toPath(), "test3",
-                ".zip").toFile();
+        this.temporaryGeneratedZipFile3 = Files.createTempFile(this.temporaryFolder.toPath(),
+            "test3",
+            ".zip").toFile();
 
-        this.temporaryGeneratedZipFile4 = Files.createTempFile(this.temporaryFolder.toPath(), "test4",
-                ".zip").toFile();
+        this.temporaryGeneratedZipFile4 = Files.createTempFile(this.temporaryFolder.toPath(),
+            "test4",
+            ".zip").toFile();
 
         try (final var zipOutputStream = new ZipOutputStream(
-                new FileOutputStream(this.temporaryGeneratedZipFile1))) {
+            new FileOutputStream(this.temporaryGeneratedZipFile1))) {
             final var zipEntry = new ZipEntry(ZIP_CONTENT_FILE_NAME1);
 
             zipOutputStream.putNextEntry(zipEntry);
         }
 
         try (final var zipOutputStream = new ZipOutputStream(
-                new FileOutputStream(this.temporaryGeneratedZipFile2))) {
+            new FileOutputStream(this.temporaryGeneratedZipFile2))) {
             final var zipEntry = new ZipEntry(ZIP_CONTENT_FILE_NAME2);
 
             zipOutputStream.putNextEntry(zipEntry);
         }
 
         try (final var zipOutputStream = new ZipOutputStream(
-                new FileOutputStream(this.temporaryGeneratedZipFile3))) {
+            new FileOutputStream(this.temporaryGeneratedZipFile3))) {
             final var zipEntry = new ZipEntry(ZIP_CONTENT_FILE_NAME3);
 
             zipOutputStream.putNextEntry(zipEntry);
         }
 
         try (final var zipOutputStream = new ZipOutputStream(
-                new FileOutputStream(this.temporaryGeneratedZipFile4))) {
+            new FileOutputStream(this.temporaryGeneratedZipFile4))) {
             final var zipEntry = new ZipEntry(ZIP_CONTENT_FILE_NAME4);
 
             zipOutputStream.putNextEntry(zipEntry);
         }
 
-        this.temporaryGeneratedOutputDirectory = Files.createDirectory(this.temporaryFolder.toPath().resolve("recOutput")).toFile();
+        this.temporaryGeneratedOutputDirectory = Files.createDirectory(
+            this.temporaryFolder.toPath().resolve("recOutput")).toFile();
 
         this.recurseZipDecompressorService = new RecurseZipDecompressorService();
     }
@@ -97,73 +103,84 @@ class RecurseZipDecompressorServiceTest {
     @DisplayName("Extract empty file should return a failure result")
     void decompressEmpty() {
         final var result = this.recurseZipDecompressorService.decompress(this.temporaryFolderEmpty,
-                this.temporaryGeneratedOutputDirectory);
+            this.temporaryGeneratedOutputDirectory);
 
         assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
-                .isEqualTo(false);
+            .isEqualTo(Boolean.FALSE);
     }
 
     @Test
     @DisplayName("Extract not existing directory should return a failure result")
     void decompressNotExistingDirectory() {
         final var result = this.recurseZipDecompressorService.decompress(new File("notExisting"),
-                this.temporaryGeneratedOutputDirectory);
+            this.temporaryGeneratedOutputDirectory);
 
         assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
-                .isEqualTo(false);
+            .isEqualTo(Boolean.FALSE);
     }
 
     @Test
     @DisplayName("Extract the zip file should return a success result")
     void decompress() {
         final var result = this.recurseZipDecompressorService.decompress(this.temporaryFolder,
-                this.temporaryGeneratedOutputDirectory);
+            this.temporaryGeneratedOutputDirectory);
 
         assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
-                .isEqualTo(true);
+            .isEqualTo(Boolean.TRUE);
     }
 
     @Test
     @DisplayName("Extract the directory in an unexisting output directory should return a success result")
     void decompressInUnexistingOutputDirectory() {
         final var result = this.recurseZipDecompressorService.decompress(this.temporaryFolder,
-                new File("truc"));
+            new File("truc"));
 
         assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
-                .isEqualTo(true);
+            .isEqualTo(true);
     }
 
     @Test
     @DisplayName("Extract the directory should unzip all directories in the output directory")
     void decompressAllDirectories() {
         final var result = this.recurseZipDecompressorService.decompress(this.temporaryFolder,
-                this.temporaryGeneratedOutputDirectory);
+            this.temporaryGeneratedOutputDirectory);
 
         assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
-                .isEqualTo(true);
+            .isEqualTo(true);
 
         assertThat(this.temporaryGeneratedOutputDirectory.listFiles()).isNotNull()
-                .hasSize(4);
+            .hasSize(4);
 
         assertThat(this.temporaryGeneratedOutputDirectory.listFiles()).extracting(File::getName)
-                .containsExactlyInAnyOrder(temporaryGeneratedZipFile1.getName().replace(".zip", ""), temporaryGeneratedZipFile2.getName().replace(".zip", ""),
-                        temporaryGeneratedZipFile3.getName().replace(".zip", ""), temporaryGeneratedZipFile4.getName().replace(".zip", ""));
+            .containsExactlyInAnyOrder(this.temporaryGeneratedZipFile1.getName().replace(".zip", ""),
+                this.temporaryGeneratedZipFile2.getName().replace(".zip", ""),
+                this.temporaryGeneratedZipFile3.getName().replace(".zip", ""),
+                this.temporaryGeneratedZipFile4.getName().replace(".zip", ""));
     }
 
     @Test
     @DisplayName("Extract the directory should correctly unzip all files in the output directory and subdirectories")
     void decompressAllFiles() {
         final var result = this.recurseZipDecompressorService.decompress(this.temporaryFolder,
-                this.temporaryGeneratedOutputDirectory);
+            this.temporaryGeneratedOutputDirectory);
 
         assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
-                .isEqualTo(true);
+            .isEqualTo(true);
 
         assertThat(this.temporaryGeneratedOutputDirectory.listFiles()).extracting(File::listFiles)
-                .extracting(files -> files[0].getName())
-                .containsExactlyInAnyOrder(ZIP_CONTENT_FILE_NAME1, ZIP_CONTENT_FILE_NAME2, ZIP_CONTENT_FILE_NAME3, ZIP_CONTENT_FILE_NAME4);
+            .extracting(files -> files[0].getName())
+            .containsExactlyInAnyOrder(ZIP_CONTENT_FILE_NAME1, ZIP_CONTENT_FILE_NAME2,
+                ZIP_CONTENT_FILE_NAME3, ZIP_CONTENT_FILE_NAME4);
     }
 
+    @Test
+    @DisplayName("Decompress recursive should return a failure result when the output directory is not a directory")
+    void decompressRecursiveOutputNotDirectory() {
+        final var result = this.recurseZipDecompressorService.decompress(this.temporaryFolder,
+            this.temporaryGeneratedZipFile1);
 
+        assertThat(result).isNotNull().isInstanceOf(Result.class).extracting(Result::success)
+            .isEqualTo(false);
+    }
 
 }
